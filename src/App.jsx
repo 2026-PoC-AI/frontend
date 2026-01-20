16ef76e8af0877395ee647529a2e5092a486c6d6
ChangHayeong <bestdev.hy@gmail.com>
2026-01-21 02:23:16 +0900

    chore(text): 뉴스 분석 페이지 라우트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import AppLayout from "./components/layout/AppLayout";
+import AppLayout from "./components/Layout/AppLayout";
 import MainPage from "./pages/MainPage";
 import AudioPage from "./pages/audio/AudioPage";
 import ImagePage from "./pages/image/ImagePage";
+import TextPage from './pages/text/TextPage';
 import ImageHistoryPage from "./pages/image/ImageHistoryPage";
 import { VideoUpload } from "./features/video/components/VideoUpload";
 import { VideoProgress } from "./features/video/components/VideoProgress";
@@ -19,6 +20,7 @@ function App() {
         <Route path="/video" element={<VideoUpload />} />
         <Route path="/video/analyzing" element={<VideoProgress />} />
         <Route path="/video/results" element={<VideoResults />} />
+        <Route path="/text" element={<TextPage />} />
       </Route>
     </Routes>
   );
